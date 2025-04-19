89f66a121974b2e9c1b4840cfd2c00ccfbf7f134
samiaaziz1457 <samiaaziz502@gmail.com>
2025-04-19 21:30:49 +0500

    4/19/25

--- lib/widgets/commontext_filed.dart
+++ lib/widgets/commontext_filed.dart
@@ -10,20 +10,24 @@ class CommontextFiled extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return TextField(
-      decoration: InputDecoration(
-        hintText: hinttext,
-        labelText: labeltext,
-        prefixIcon: Icon(prefixicons),
-        hintStyle: TextStyle(color: Colors.grey[700]),
-    filled: true,
-    fillColor: Colors.white,
-     contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(30),
-      borderSide: BorderSide.none,
-    ),
-  ),
-    );
+    return
+     Padding(
+       padding: const EdgeInsets.symmetric(horizontal: 20),
+       child: TextField(
+        decoration: InputDecoration(
+          hintText: hinttext,
+          labelText: labeltext,
+          prefixIcon: Icon(prefixicons),
+          hintStyle: TextStyle(color: Colors.grey[700]),
+           filled: true,
+           fillColor: Colors.white,
+       contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+           border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide.none,
+           ),
+         ),
+           ),
+     );
   }
 }
